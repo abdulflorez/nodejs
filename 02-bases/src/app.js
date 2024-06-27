b21b1669791a9f7f176417558e81e8a7c63c7015
Abdul Florez <abdulflorez1524@gmail.com>
2024-06-26 21:28:39 -0500

    refactor promises

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -28,4 +28,7 @@
 // ---------- 06
 const { getPokemonById } = require('./js-foundation/06-promises');
 
-getPokemonById(1);
+getPokemonById(1)
+  .then((pokemon) => console.log({ pokemon }))
+  .catch((err) => console.error(err))
+  .finally(() => console.log('Finalmente'));
--- 02-bases/src/js-foundation/06-promises.js
+++ 02-bases/src/js-foundation/06-promises.js
@@ -2,7 +2,7 @@ const { http } = require('../plugins');
 
 const getPokemonById = async (id) => {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-  return console.log((await http.get(url)).name);
+  return (await http.get(url)).name;
 };
 
 module.exports = {
