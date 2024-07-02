9de78342366302888a3f43a6954ba2f19f56ef24
Abdul Florez <abdulflorez1524@gmail.com>
2024-07-01 20:31:26 -0500

    Adding logs for project

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -26,9 +26,17 @@
 // console.log(john);
 
 // ---------- 06
-const { getPokemonById } = require('./js-foundation/06-promises');
+// const { getPokemonById } = require('./js-foundation/06-promises');
 
-getPokemonById(1)
-  .then((pokemon) => console.log({ pokemon }))
-  .catch((err) => console.error(err))
-  .finally(() => console.log('Finalmente'));
+// getPokemonById(1)
+//   .then((pokemon) => console.log({ pokemon }))
+//   .catch((err) => console.error(err))
+//   .finally(() => console.log('Finalmente'));
+
+// ----------
+const { buildLogger } = require('./plugins');
+
+const logger = buildLogger('app.js');
+
+logger.log('Hola Mundo');
+logger.error('Something went wrong');
